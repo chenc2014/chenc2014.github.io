98343d80bd64e98c4472aafa5396e657c4785a23
chenc2014 <85598476+chenc2014@users.noreply.github.com>
2024-01-06 16:04:13 +0800

    Create myScript.js

---
+++ myScript.js
@@ -0,0 +1,22 @@
+var divs = ["Div1", "Div2", "Div3", "Div4"];
+    var visibleDivId = null;
+    function divVisibility(divId) {
+      if(visibleDivId === divId) {
+        visibleDivId = null;
+      } else {
+        visibleDivId = divId;
+      }
+      hideNonVisibleDivs();
+    }
+    function hideNonVisibleDivs() {
+      var i, divId, div;
+      for(i = 0; i < divs.length; i++) {
+        divId = divs[i];
+        div = document.getElementById(divId);
+        if(visibleDivId === divId) {
+          div.style.display = "block";
+        } else {
+          div.style.display = "none";
+        }
+      }
+    }
